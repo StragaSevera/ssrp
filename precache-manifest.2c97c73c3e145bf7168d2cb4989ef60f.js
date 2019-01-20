99d1fec42a126cd6167803eca14f12a9a8ec1d98
Straga Severa <ought@yandex.ru>
2019-01-20 04:12:33 +0300

    Updates

---
+++ precache-manifest.2c97c73c3e145bf7168d2cb4989ef60f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "341f9576ceaa0fa36acb",
+    "url": "/ssrp/static/css/main.f33f5332.chunk.css"
+  },
+  {
+    "revision": "341f9576ceaa0fa36acb",
+    "url": "/ssrp/static/js/main.341f9576.chunk.js"
+  },
+  {
+    "revision": "5c6d45d5a46095f549a5",
+    "url": "/ssrp/static/js/1.5c6d45d5.chunk.js"
+  },
+  {
+    "revision": "07133201df77daf6b114",
+    "url": "/ssrp/static/js/runtime~main.07133201.js"
+  },
+  {
+    "revision": "1415712d7451c986e9dff2d5b2d30abb",
+    "url": "/ssrp/index.html"
+  }
+];
